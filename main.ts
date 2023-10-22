13816b045a85846caec613e2bc3c106705fe798d
Rubens Pischedda <rubens.pischedda@bitapp.it>
2023-10-22 23:44:39 +0200

    feat: implement table view

--- main.ts
+++ main.ts
@@ -1,134 +1,231 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Plugin } from "obsidian";
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
+export default class JsonInlineFieldsPlugin extends Plugin {
 	async onload() {
-		await this.loadSettings();
+		app.workspace.trigger("parse-style-settings");
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		this.registerMarkdownPostProcessor((el, ctx) => {
+			if (
+				el.parentElement?.classList?.contains(
+					"inline-field-standalone-value"
+				) ||
+				el.parentElement?.classList?.contains("inline-field-value")
+			) {
+				const isStandalone = el.parentElement?.classList?.contains(
+					"inline-field-standalone-value"
+				);
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+				try {
+					// Parse metadata value. If it's not a JSON, it will throw an error
+					// and the default rendering will be used.
+					let inlineFieldKey: string | undefined;
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
+					if (!isStandalone) {
+						inlineFieldKey =
+							(
+								el.parentElement?.parentElement?.querySelector(
+									".inline-field-key"
+								) as HTMLElement
+							).getAttribute("data-dv-key") ?? undefined;
 					}
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+					const inlineFieldValue = JSON.parse(el.innerText);
+					
+					this.addLinksToInlineFieldsValues(el, inlineFieldValue);
+
+					// Create a container for the table(s)
+					const container = this.createContainerElement();
+
+					// If it's an array, create a table for each item, otherwise create a single table
+					const values = [inlineFieldValue].flat();
+
+					if (values.length > 1 && inlineFieldKey) {
+						const titleElement = document.createElement("div");
+						titleElement.innerText = inlineFieldKey;
+						titleElement.addClass("json-inline-fields-title");
+						container.appendChild(titleElement);
+					}
+
+					const tables: HTMLElement[] = [];
+
+					values.forEach((item: any, index: number, array: any[]) => {
+						const title =
+							(array.length === 1 && inlineFieldKey) || undefined;
+						const table = this.createTableElement(
+							item,
+							false,
+							title
+						);
+						tables.push(table);
+					});
+
+					if (tables.length > 1) {
+						const tablesContainer = document.createElement("div");
+						tablesContainer.addClass(
+							"json-inline-fields-tables-container"
+						);
+						tables.forEach((table) => {
+							tablesContainer.appendChild(table);
+						});
+						container.appendChild(tablesContainer);
+					} else {
+						container.appendChild(tables[0]);
+					}
+
+					// Replace the standard inline field rendering
+					if (!isStandalone) {
+						const keyElement =
+							el.parentElement?.parentElement?.querySelector(
+								".inline-field-key"
+							) as HTMLElement;
+
+						el.parentElement?.parentElement?.removeChild(
+							keyElement
+						);
+					}
+
+					el.parentElement?.replaceWith(container);
+				} catch (e) {
+					// not a json
 				}
 			}
 		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
-	onunload() {
-
+	addLinksToInlineFieldsValues(el: HTMLElement, inlineFieldValue: any) {
+		//TODO: inject links to the inline field values
+		return inlineFieldValue;
 	}
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	onunload() {}
+
+	/**
+	 * Creates a new container element
+	 * @returns The newly created container element.
+	 */
+	createContainerElement(): HTMLElement {
+		const container = document.createElement("div");
+		container.addClass("json-inline-fields-container");
+		return container;
 	}
 
-	async saveSettings() {
-		await this.saveData(this.settings);
+	/**
+	 * Creates a new table element
+	 * @returns The newly created table element.
+	 */
+	createTableElement(
+		object: any,
+		innerTable: boolean = false,
+		title?: string
+	): HTMLElement {
+		const table = document.createElement("div");
+		table.addClass("json-inline-fields-table");
+		if (innerTable) {
+			table.addClass("json-inline-fields-inner-table");
+		}
+
+		if (title) {
+			const tableTitleContainer = document.createElement("div");
+			tableTitleContainer.addClass(
+				"json-inline-fields-table-title-container"
+			);
+
+			const tableTitle = document.createElement("div");
+			tableTitle.addClass("json-inline-fields-table-title");
+			tableTitle.innerText = title;
+
+			tableTitleContainer.appendChild(tableTitle);
+			table.appendChild(tableTitleContainer);
+		}
+
+		this.populateTable(table, object);
+		return table;
 	}
-}
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
+	/**
+	 * Creates a new table row key element
+	 * @param property The property to be used as the key.
+	 * @returns The newly created table row key element.
+	 */
+	createRowKeyElement(property: any): HTMLElement {
+		const rowKey = document.createElement("div");
+		rowKey.classList.add("json-inline-fields-key");
+		rowKey.innerText = property;
+		return rowKey;
 	}
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
+	/**
+	 * Creates a new table row value element
+	 * @param object The whole object.
+	 * @param property The property to get the value for.
+	 */
+	createRowValueElement(object: any, property: any): HTMLElement {
+		const rowValue = document.createElement("div");
+		rowValue.classList.add("json-inline-fields-value");
+
+		if (Array.isArray(object[property])) {
+			rowValue.classList.add("json-inline-fields-array");
+			object[property].forEach((item: any) => {
+				if (typeof item === "object" || Array.isArray(item)) {
+					const table = this.createTableElement(item, true);
+					rowValue.appendChild(table);
+				} else {
+					const rowArrayValue = document.createElement("div");
+					rowArrayValue.classList.add(
+						"json-inline-fields-array-value"
+					);
+					rowArrayValue.innerText = item;
+					rowValue.appendChild(rowArrayValue);
+				}
+			});
+		} else {
+			if (typeof object[property] === "object") {
+				const table = this.createTableElement(object[property], true);
+				rowValue.appendChild(table);
+			} else {
+				rowValue.innerText = object[property];
+			}
+		}
+		return rowValue;
 	}
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
+	/**
+	 * Creates a new table row element
+	 * @param object The whole object.
+	 * @param property The property to create the row for.
+	 * @returns The newly created table row element.
+	 */
+	createTableRowElement(object: any, property: any): { key: HTMLElement, value: HTMLElement } {
+		// Create the row element
+		// const rowElement = document.createElement("div");
+		// rowElement.addClass("json-inline-fields-row");
+
+		// Create the key element
+		const rowKey = this.createRowKeyElement(property);
+
+		// Create the value element
+		const rowValue = this.createRowValueElement(object, property);
+
+		// rowElement.appendChild(rowKey);
+		// rowElement.appendChild(rowValue);
+
+		return { key: rowKey, value: rowValue };
 	}
-}
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
+	/**
+	 * Populates the given table element with rows for each property in the given object.
+	 * @param container The table element to populate.
+	 * @param object The object to populate the table with.
+	 */
+	populateTable(container: HTMLElement, object: any) {
+		for (const property in object) {
+			const row = this.createTableRowElement(object, property);
+			container.appendChild(row.key);
+			container.appendChild(row.value);
+		}
 	}
 
-	display(): void {
-		const {containerEl} = this;
+	async loadSettings() {}
 
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+	async saveSettings() {}
 }
